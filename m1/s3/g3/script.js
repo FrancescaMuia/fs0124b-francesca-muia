3f5a1e8f36a1cd0c6ed524cbeba942b68a9e2618
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-02-07 19:52:23 +0100

    tutto esercizio

--- m1/s3/g3/script.js
+++ m1/s3/g3/script.js
@@ -3,13 +3,20 @@ let btn = document.querySelector("#btn");
 btn.addEventListener("click", addTask);
 
 function addTask() {
-  let newDiv = document.createElement("div");
+  let li = document.createElement("li");
   let deleteBtn = document.createElement("button");
   deleteBtn.innerText = "Elimina";
-  deleteBtn.addEventListener("click");
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+  });
 
   let value = document.getElementById("input").value;
-  newDiv.innerText = value;
+  li.innerText = value;
+  li.appendChild(deleteBtn);
 
-  document.querySelector("#target").append(newDiv, deleteBtn);
+  li.addEventListener("click", function () {
+    li.classList.toggle("fatto");
+  });
+
+  document.querySelector("#target").append(li);
 }
