95eee9c4d5a5116b29066bcfa05b657b02cd22c5
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-02-08 15:23:42 +0100

    miracolo, ho creato le caselle

--- m1/s3/g4/js/script.js
+++ m1/s3/g4/js/script.js
@@ -1,1 +1,14 @@
-const numeriCaselle = document.querySelector(".caselle");
+const numeriCaselle = document.querySelector(".contentCaselle");
+
+//ciclo per avere i numeri
+for (let i = 1; i <= 76; i++) {
+  //creo i div con i numeri e assegno la classe
+  const numbers = document.createElement("div");
+  numbers.classList.add("caselle");
+
+  //inserisco numeri nelle caselle
+  numbers.innerText = i;
+
+  //appendo le caselle al contenitore per vederle
+  numeriCaselle.append(numbers);
+}
