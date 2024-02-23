79a5637ed5da1051facf41dc6d576ec123cfe535
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-02-23 17:50:58 +0100

    add color btn

--- m2/s1/g5/base_di_partenza/assets/script/script.js
+++ m2/s1/g5/base_di_partenza/assets/script/script.js
@@ -1,10 +1,13 @@
 window.addEventListener("scroll", function () {
-  var navbar = document.getElementById("navbar");
-  var scrollPosition = window.scrollY;
+  let navbar = document.getElementById("navbar");
+  let button = document.getElementById("btn");
+  let scrollPosition = window.scrollY;
 
   if (scrollPosition > 300) {
     navbar.classList.add("sticky");
+    button.classList.add("green");
   } else {
     navbar.classList.remove("sticky");
+    button.classList.remove("green");
   }
 });
