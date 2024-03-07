0e90835000f1b744d9683c2b66374e54a73901dd
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-03-07 15:53:21 +0100

    es foto

---
+++ m2/s3/g4/script.js
@@ -0,0 +1,25 @@
+async function chiamata(query) {
+  const apiKey = "sUJ9ljZG3us09kZlcRU483nIwGGPJqPg7nTHDYD9X6Lkf7ymNa3zVJX6";
+
+  return await fetch(`https://api.pexels.com/v1/search?query=${query}`, {
+    method: "GET",
+    headers: {
+      Authorization: apiKey,
+    },
+  }).then((dati) => dati.json());
+}
+
+// chiamata("cat").then((cats) => {
+//   console.log(cats);
+// });
+
+let button = document.querySelector(".my-2");
+button.addEventListener("click", () => {
+  chiamata("cat").then((dati) => {
+    console.log(dati);
+    let img = document.querySelectorAll(".card-img-top");
+    dati.photos.forEach((photo, index) => {
+      img[index].src = photo.src.medium;
+    });
+  });
+});
