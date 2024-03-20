673f6211c2b29f1558aff0a0bbdc496fb4b3cc61
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-03-20 19:29:58 +0100

    random+check no stesso articolo

--- m3/s1/g3/angular2/src/app/app.component.ts
+++ m3/s1/g3/angular2/src/app/app.component.ts
@@ -8,10 +8,25 @@ import { Articolo } from './Models/articolo';
 })
 export class AppComponent {
   articoliArray: Articolo[] = [];
+  articoliRandom: Articolo[] = [];
 
   ngOnInit() {
     fetch('../assets/db.json')
       .then((articoli) => articoli.json())
-      .then((articolo) => (this.articoliArray = articolo.posts));
+      .then((articolo) => {
+        this.articoliArray = articolo.posts;
+        this.soloQuattroRandom();
+      });
+  }
+
+  soloQuattroRandom(): void {
+    for (let i = 0; i < 4; i++) {
+      let random = Math.floor(Math.random() * this.articoliArray.length);
+      this.articoliRandom.push(this.articoliArray[random]);
+      //per non ripetere stesso articolo
+      this.articoliArray = this.articoliArray.filter(
+        (articolo) => articolo !== this.articoliArray[random]
+      );
+    }
   }
 }
