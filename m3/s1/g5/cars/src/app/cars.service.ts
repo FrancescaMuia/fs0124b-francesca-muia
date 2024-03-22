a09c6f14576c5587437d38b2489dfb164b825012
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-03-22 12:50:17 +0100

    home con card random colorate

--- m3/s1/g5/cars/src/app/cars.service.ts
+++ m3/s1/g5/cars/src/app/cars.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { iCar } from './Models/i-car';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CarsService {
+  carArray: iCar[] = [];
+  carRandom: iCar[] = [];
 
-  constructor() { }
+  getCars(): Promise<iCar[]> {
+    return fetch('../assets/db.json')
+      .then((res) => res.json())
+      .then((res: iCar[]) => res);
+  }
+
+  getAudi(): Promise<iCar[]> {
+    return this.getCars().then((res) => res.filter((c) => c.brand === 'Audi'));
+  }
+
+  getFiat(): Promise<iCar[]> {
+    return this.getCars().then((res) => res.filter((c) => c.brand === 'Fiat'));
+  }
+
+  getFord(): Promise<iCar[]> {
+    return this.getCars().then((res) => res.filter((c) => c.brand === 'Ford'));
+  }
+
+  soloDueRandom(): void {
+    for (let i = 0; i < 2; i++) {
+      let random = Math.floor(Math.random() * this.carArray.length);
+      this.carRandom.push(this.carArray[random]);
+      //per non ripetere stesso articolo
+      this.carArray = this.carArray.filter(
+        (car) => car !== this.carArray[random]
+      );
+    }
+  }
 }
--- m3/s1/g5/cars/src/app/pages/home/home.component.ts
+++ m3/s1/g5/cars/src/app/pages/home/home.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { iCar } from '../../Models/i-car';
+import { CarsService } from '../../cars.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  carArray: iCar[] = [];
+  carRandom: iCar[] = [];
 
+  constructor(private carSvc: CarsService) {}
+
+  ngOnInit() {
+    // this.carSvc.getCars().then((res) => {
+    //   this.carArr = res;
+    // });
+    this.carSvc.getCars().then((res) => {
+      // this.articoliRandom = res;
+      // console.log(this.articoliRandom);
+      for (let i = 0; i < 2; i++) {
+        let random = Math.floor(Math.random() * this.carArray.length);
+        this.carRandom.push(res[random]);
+        // per non ripetere stesso articolo
+        this.carArray = res.filter((car) => car !== this.carArray[random]);
+      }
+    });
+    console.log(this.carArray);
+  }
 }
