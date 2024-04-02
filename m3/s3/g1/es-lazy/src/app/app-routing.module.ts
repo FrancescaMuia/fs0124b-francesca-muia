de24b9158eff3b77db0ac2974747e129a13601cd
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-04-02 14:47:16 +0200

    lazy loading

---
+++ m3/s3/g1/es-lazy/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'login', loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule) }, { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
