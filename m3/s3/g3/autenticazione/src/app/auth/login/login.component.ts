a98105f9792c0334aa84f266ec90750bec9daa6a
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-04-04 16:26:02 +0200

    add moduli+componenti

--- m3/s3/g3/autenticazione/src/app/auth/login/login.component.ts
+++ m3/s3/g3/autenticazione/src/app/auth/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { LoginData } from '../../Models/login-data';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginData: LoginData = {
+    email: 'mario@varano.it',
+    password: 'password',
+  };
 
+  constructor(private authSvc: AuthService, private router: Router) {}
+
+  signIn() {
+    this.authSvc.login(this.loginData).subscribe((data) => {
+      this.router.navigate(['/dashboard']);
+    });
+  }
 }
--- m3/s3/g3/autenticazione/src/app/auth/register/register.component.ts
+++ m3/s3/g3/autenticazione/src/app/auth/register/register.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { IUser } from '../../Models/i-user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  registerData: Partial<IUser> = {};
 
+  constructor(private authSvc: AuthService, private router: Router) {}
+
+  signUp() {
+    this.authSvc.register(this.registerData).subscribe((data) => {
+      this.router.navigate(['dashboard']);
+    });
+  }
 }
