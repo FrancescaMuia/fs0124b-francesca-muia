a33cdca662f0a0e335c9282dd5c16cf83827372c
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-04-04 15:23:54 +0200

    autenticazione

--- m3/s3/g2/form/src/app/components/login/login.component.ts
+++ m3/s3/g2/form/src/app/components/login/login.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  @ViewChild('f', { static: true }) form!: NgForm;
 
+  submit(form: NgForm) {
+    if (form.valid) {
+      console.log('Dati del login:', form.value);
+    }
+  }
 }
