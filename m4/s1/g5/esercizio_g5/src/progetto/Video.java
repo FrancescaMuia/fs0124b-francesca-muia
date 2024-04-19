4fb87637cf8eca28d71c0a17ca74487a991beae9
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-04-19 15:10:14 +0200

    progetto g5 commit video terminata con metodi alza/abbassa e aumenta/diminuisci

--- m4/s1/g5/esercizio_g5/src/progetto/Video.java
+++ m4/s1/g5/esercizio_g5/src/progetto/Video.java
@@ -2,14 +2,46 @@ package progetto;
 
 public class Video extends ElementoMultimediale {
 
-    public Video(String titolo, int durata) {
+    //aggiungo nuovi attributi di video
+    private int volume;
+    private int luminosita;
+
+    public Video(String titolo, int durata, int volume, int luminosita) {
         //richiamo costruttore della classe padre
         super(titolo, durata);
+        this.volume = volume;
+        this.luminosita = luminosita;
     }
 
     @Override
     public void play() {
-        System.out.println("Riproduco video: " + titolo);
-        //codice che mi permette di riprodurre video
+        for(int i = 0; i< durata; i++){
+            String seqPuntiEsclamativi = "!".repeat(volume);
+            String seqAsterischi = "*".repeat(luminosita);
+            System.out.println(titolo + " " + seqPuntiEsclamativi + seqAsterischi);
+        }
+    }
+
+    public void aumentaLum(){
+        luminosita++;
+    }
+
+    public void diminuisciLum(){
+        if(luminosita>0){
+            luminosita--;
+        }
+    }
+
+    public void alzaVolume(){
+        //alzo il volume
+        volume++;
+    }
+
+    public void abbassaVolume(){
+        //se volume maggiore di 0 lo diminuisce di 1
+        if(volume>0){
+            volume--;
+        }
     }
 }
+
