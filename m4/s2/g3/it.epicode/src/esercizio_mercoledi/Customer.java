23202c84bbc2cdc55ee57ef4230ef7dc7af216e8
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2024-04-24 16:20:10 +0200

    es merc ottengo lista prodotti filtrati

---
+++ m4/s2/g3/it.epicode/src/esercizio_mercoledi/Customer.java
@@ -0,0 +1,46 @@
+package esercizio_mercoledi;
+
+public class Customer {
+    private long id;
+    private String name;
+    private Integer tier;
+
+    public Customer(Integer tier, String name, long id) {
+        this.tier = tier;
+        this.name = name;
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getTier() {
+        return tier;
+    }
+
+    public void setTier(Integer tier) {
+        this.tier = tier;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tier=" + tier +
+                '}';
+    }
+}
