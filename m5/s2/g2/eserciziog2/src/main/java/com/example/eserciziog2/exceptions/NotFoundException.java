1e5c7386201e8cb6f6079613909d5df306b66bac
FrancescaMuia <francesca.muia91@gmail.com>
2024-05-22 16:05:06 +0200

    add

--- m5/s2/g2/eserciziog2/src/main/java/com/example/eserciziog2/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.eserciziog2.exceptions;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(long id) {
-        super("Entity with id " + id + " not found");
-    }
-}
