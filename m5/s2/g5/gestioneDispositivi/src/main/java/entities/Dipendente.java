c5c75e9d4faaf03ef3642a4f5d2a8166d02fd899
FrancescaMuia <francesca.muia91@gmail.com>
2024-05-24 12:39:34 +0200

    progetto g5

---
+++ m5/s2/g5/gestioneDispositivi/src/main/java/entities/Dipendente.java
@@ -0,0 +1,44 @@
+package entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Entity
+@Data
+@Table(name = "dipendenti")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dipendente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @NotBlank
+    @NotEmpty
+    @NotNull
+    private String nome;
+    @NotBlank
+    @NotEmpty
+    @NotNull
+    private String cognome;
+    @NotBlank
+    @NotEmpty
+    @NotNull
+    @Email
+    private String email;
+    @NotBlank
+    @NotEmpty
+    @NotNull
+    private String userName;
+
+    @OneToMany(mappedBy = "dipendente")
+    private List<Dispositivo> dispositivi;
+}
---
+++ m5/s2/g5/gestioneDispositivi/src/main/java/entities/Dispositivo.java
@@ -0,0 +1,27 @@
+package entities;
+
+import enums.StatoDispositivo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "dispositivi")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dispositivo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String tipologia;
+
+    @Enumerated(EnumType.STRING)
+    private StatoDispositivo stato;
+
+    @ManyToOne
+    private Dipendente dipendente;
+}
